00ee190b93b200a9d2869396b7770e3ba9c1a38d
Truong Hoang Dung <checkraiser11@gmail.com>
2017-06-11 12:23:13 +0700

    Create image.rb

--- lib/froala-editor-sdk/image.rb
+++ lib/froala-editor-sdk/image.rb
@@ -25,7 +25,7 @@ module FroalaEditorSDK
       all_images = []
 
       images.each do |img|
-        all_images.push({url: "#{"/uploads/"}#{Utils.get_file_name(img)}"})
+        all_images.push({url: "#{"/uploads/images/"}#{Utils.get_file_name(img)}"})
       end
 
       return all_images.to_json
